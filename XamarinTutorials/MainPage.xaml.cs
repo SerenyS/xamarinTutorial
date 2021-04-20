e0be282cecc6fbe267fd5387972f1724daa4fcd3
Sereny Sanchez <52845840+SerenyS@users.noreply.github.com>
2021-04-20 01:28:30 -0500

    Lesson #4 - Lab B

---
+++ XamarinTutorials/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinTutorials
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void clickedSong(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Plain White T's - 1, 2, 3, 4";
+        }
+
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+        }
+
+        void OnEntryCompleted(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
+    }
+}
